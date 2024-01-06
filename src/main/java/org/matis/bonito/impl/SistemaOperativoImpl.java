1b07676f69090987fe54c993f4e09e01d33d18ec
oscar <slogant@gmail.com>
2024-01-05 21:17:10 -0600

    Merge remote-tracking branch 'save_local/master'

--- src/main/java/org/matis/bonito/impl/SistemaOperativoImpl.java
+++ src/main/java/org/matis/bonito/impl/SistemaOperativoImpl.java
@@ -1,25 +1,23 @@
 package org.matis.bonito.impl;
 
-import org.matis.bonito.model.Sedes;
 import org.matis.bonito.model.SistemaOperativo;
 
-import java.util.List;
 import java.util.stream.Stream;
 
 public interface SistemaOperativoImpl {
-    public default boolean crearSistemaOperativo(SistemaOperativo sistemaOperativo) {
+    default boolean crearSistemaOperativo(SistemaOperativo sistemaOperativo) {
         return false;
     }
 
-    public default boolean EliminarSistemaOperativo(Long id_sistema) {
+    default boolean EliminarSistemaOperativo(Long id_sistema) {
         return false;
     }
 
-    public default boolean actualizaSistemaOperativo(SistemaOperativo sistemaOperativo) {
+    default boolean actualizaSistemaOperativo(SistemaOperativo sistemaOperativo) {
         return false;
     }
 
-    public default Stream<SistemaOperativo> obtenerSistemaOperativo() {
+    default Stream<SistemaOperativo> obtenerSistemaOperativo() {
         return null;
     }
 }
