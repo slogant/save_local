292c048e0271ce56f9b475abd83b8e1f320767d4
oscar <slogant@gmail.com>
2024-02-03 19:35:03 -0600

    Merge remote-tracking branch 'save_local/master'

--- src/main/java/org/matis/bonito/model/Sedes.java
+++ src/main/java/org/matis/bonito/model/Sedes.java
@@ -2,7 +2,6 @@ package org.matis.bonito.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
